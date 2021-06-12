7923ff66672115015c8698849a3ed77024d719a3
Matthew Casperson <matthewcasperson@gmail.com>
2021-06-12 15:01:00 +1000

    working on initial example

---
+++ src/Application.kt
@@ -0,0 +1,63 @@
+package com.mathewceron
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+        oauth("auth0") {
+            client = HttpClient(Apache)
+            providerLookup = {
+                OAuthServerSettings.OAuth2ServerSettings(
+                    name = "auth0",
+                    authorizeUrl = "https://matthewcasperson.au.auth0.com/login/oauth/authorize",
+                    accessTokenUrl = "https://matthewcasperson.au.auth0.com/oauth/access_token",
+                    clientId = System.getenv("CLIENT_ID"),
+                    clientSecret = System.getenv("CLIENT_SECRET")
+                )
+            }
+            urlProvider = { "/login/auth0" }
+        }
+    }
+
+    routing {
+        get("/api/messages/public") {
+            call.respondText(
+                """{"message": "The API doesn't require an access token to share this message."}""",
+                contentType = ContentType.Application.Json)
+        }
+    }
+
+    routing {
+        authenticate("auth0") {
+            get("/api/messages/protected") {
+                call.respondText(
+                    """{"message": "The API successfully validated your access token."}""",
+                    contentType = ContentType.Application.Json
+                )
+            }
+        }
+    }
+
+    routing {
+        authenticate("auth0") {
+            get("/api/messages/admin") {
+                call.respondText(
+                    """"{"message": "The API successfully recognized you as an admin."}""",
+                    contentType = ContentType.Application.Json
+                )
+            }
+        }
+    }
+}
+
