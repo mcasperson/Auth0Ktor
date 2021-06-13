2111e5bbedf75a54e6e255da636781cc42f0d62b
Matthew Casperson <matthewcasperson@gmail.com>
2021-06-13 16:13:32 +1000

    deall with null

--- src/KeyUtils.kt
+++ src/KeyUtils.kt
@@ -6,9 +6,9 @@ import java.security.cert.X509Certificate
 import java.security.interfaces.RSAPublicKey
 
 
-fun readPublicKey(key: InputStream): RSAPublicKey {
+fun readPublicKey(key: InputStream?): RSAPublicKey? {
+    if (key == null) return null
     val certFactory: CertificateFactory = CertificateFactory.getInstance("X.509")
     val cer: X509Certificate = certFactory.generateCertificate(key) as X509Certificate
     return cer.publicKey as RSAPublicKey
-
 }
