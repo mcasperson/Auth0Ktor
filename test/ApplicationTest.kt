f5b8df1e43bd7a8d3792aa30946bb2761fb9b3b7
Matthew Casperson <matthewcasperson@gmail.com>
2021-06-14 15:53:30 +1000

    Reformatted file

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,14 +1,10 @@
 package com.mathewceron
 
 import com.matthewcasperson.module
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.auth.*
-import kotlin.test.*
 import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
