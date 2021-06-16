1ef2312ba62ccb9cb020cf13b648efe3affd1b40
Matthew Casperson <matthewcasperson@gmail.com>
2021-06-16 15:35:17 +1000

    Used allowCredentials = true instead

--- src/Application.kt
+++ src/Application.kt
@@ -39,7 +39,6 @@ fun Application.module() {
         anyHost()
         method(HttpMethod.Options)
         method(HttpMethod.Get)
-        header("authorization")
         allowCredentials = true
         allowNonSimpleContentTypes = true
 
